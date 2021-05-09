bff5d1521e717b4e102ea0d84d1d4e6b5484a705
Grace <grace@swapreads.com>
2021-05-09 10:26:02 +0300

    initial route setup

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+
+  components: {},
+
+  data: function () {
+    return {
+      title: "Dashboard",
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+  data: function () {
+    return {
+      title: "Login",
+    };
+  },
+};
+</script>
+
+<style></style>
